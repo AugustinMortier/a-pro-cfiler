439a11dbea657d788f3156078ba2634dcd03fb77
augustinm <augustinm@met.no>
2024-09-20 16:52:11 +0200

    preserve time attrs

--- a_pro_cfiler/utils.py
+++ a_pro_cfiler/utils.py
@@ -1,4 +1,6 @@
 def _convert_time(ds):
+    time_attrs = ds["time"].attrs
     ds['time'] = (ds['time'] / (1000 * 60 * 60 * 24) )
+    ds["time"] = ds["time"].assign_attrs(time_attrs)
     ds["time"].attrs['units'] = 'days since 1970-01-01T00:00:00'
     return ds
